dbb99a7b9b3d8e1ade1d30095cdf5d75efdf0358
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2024-10-25 10:35:00 +0700

    contact detail

--- koi-vet-app/src/pages/ContactManagement/ContactManagement.jsx
+++ koi-vet-app/src/pages/ContactManagement/ContactManagement.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { Table } from 'antd'
-import { fetchContactAPI } from '../../apis'    
-// import Modal from '../../components/Modal/Modal';
+import { fetchContactAPI, fetchContactDetailAPI } from '../../apis'    
+import Modal from '../../components/Modal/Modal';
 
 function ContactManagement() {
     const [dataSource, setDataSource] = useState([]);
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // const [contactDetail, setContactDetail] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [contactDetail, setContactDetail] = useState(null);
     // const [selectedId, setSelectedId] = useState(null);
     
-    // const handleOpenModal = async (id) => {
-    //     // setSelectedId(id);
-    //     setIsModalOpen(true);
-    //     try {
-    //         const response = await fetchContactDetailAPI(id);
-    //         setContactDetail(response.data);
-    //     } catch (error) {
-    //         console.error('Error fetching contact detail:', error);
-    //     }
-    // }
+    const handleOpenModal = async (id) => {
+        // setSelectedId(id);
+        setIsModalOpen(true);
+        try {
+            const response = await fetchContactDetailAPI(id);
+            setContactDetail(response.data);
+        } catch (error) {
+            console.error('Error fetching contact detail:', error);
+        }
+    }
 
     useEffect(() => {
         const fetchContact = async () => {
@@ -48,45 +48,45 @@ function ContactManagement() {
         {
             title: "Action",
             key: "action",
-            // render: (_, record) => (
-            //     <button onClick={() => handleOpenModal(record.id)}><i className="bi bi-box-arrow-in-right"></i></button>
-            // )
+            render: (_, record) => (
+                <button onClick={() => handleOpenModal(record.id)}><i className="bi bi-box-arrow-in-right"></i></button>
+            )
         }
     ]
 
-    // const columns2 = [
-    //     {
-    //         title: "Name",
-    //         dataIndex: "name",
-    //         key: "name",
-    //     },
-    //     {
-    //         title: "Email",
-    //         dataIndex: "email",
-    //         key: "email",
-    //     },
-    //     {
-    //         title: "Subject",
-    //         dataIndex: "subject",
-    //         key: "subject",
-    //     },
-    //     {
-    //         title: "Message",
-    //         dataIndex: "message",
-    //         key: "message",
-    //     },
-    // ]
+    const columns2 = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email",
+        },
+        {
+            title: "Subject",
+            dataIndex: "subject",
+            key: "subject",
+        },
+        {
+            title: "Message",
+            dataIndex: "message",
+            key: "message",
+        },
+    ]
 
     return (
         <Container>
             <Table dataSource={dataSource} columns={columns1} rowKey="id" pagination={{ pageSize: 7 }}/>
         
-            {/* <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
                 <h3><strong>Contact Detail</strong></h3>
                 {contactDetail && (
                     <Table dataSource={[contactDetail]} columns={columns2} rowKey="id" pagination={false}/>
                 )}
-            </Modal> */}
+            </Modal>
         </Container>
     )
 }
